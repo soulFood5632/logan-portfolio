dcffa2d94fb6dd9bdca804a2530460bc39f3955f
soulFood5632 <loganu@shaw.ca>
2023-10-02 17:20:59 -0700

    small updates

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Particles as ReactParticles } from "react-particles-js";
+// import { Particles as ReactParticles } from "react-particles-js";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   particlesCanvas: {
     position: "fixed",
-    opacity: "0.2"
+    opacity: "0.1"
   },
 }));
 
@@ -13,47 +13,48 @@ const Particles = () => {
   const classes = useStyles();
 
   return (
-    <ReactParticles
-      canvasClassName={classes.particlesCanvas}
-      params={{
-        particles: {
-          number: {
-            value: 25,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 1,
-              color: "tan",
-            },
-          },
-          size: {
-            value: 8,
-            random: true,
-            anim: {
-              enable: false,
-              speed: 4,
-              size_min: 0,
-              sync: false,
-            },
-          },
-          opacity: {
-            value: 0.5,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-            },
-          },
-        },
-      }}
-    />
+      <div></div>
+    // <ReactParticles
+    //   canvasClassName={classes.particlesCanvas}
+    //   params={{
+    //     particles: {
+    //       number: {
+    //         value: 25,
+    //         density: {
+    //           enable: true,
+    //           value_area: 800,
+    //         },
+    //       },
+    //       shape: {
+    //         type: "circle",
+    //         stroke: {
+    //           width: 1,
+    //           color: "tan",
+    //         },
+    //       },
+    //       size: {
+    //         value: 8,
+    //         random: true,
+    //         anim: {
+    //           enable: false,
+    //           speed: 4,
+    //           size_min: 0,
+    //           sync: false,
+    //         },
+    //       },
+    //       opacity: {
+    //         value: 0.5,
+    //         random: true,
+    //         anim: {
+    //           enable: true,
+    //           speed: 1,
+    //           opacity_min: 0.1,
+    //           sync: false,
+    //         },
+    //       },
+    //     },
+    //   }}
+    // />
   );
 };
 
